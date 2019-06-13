ccf22b4c86b1b3e6495b1bb28e3ad430350ce3da
Arshia <35062789+Arshiaprg@users.noreply.github.com>
2019-06-13 21:07:14 +0430

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='kms',  
+     version='1',
+     author="Ar$h1a",
+     author_email="1arhacker1@gmail.com",
+     description="A Hacking Library That Helps You To Create Hacking Tools.",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/ArshiaPRG/KMS",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
