5ebe2e62d0e34073f1034c1018444bd1666313d5
Arshia <35062789+Arshiaprg@users.noreply.github.com>
2019-07-18 02:01:19 -0700

    Setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='kms',  
-     version='1',
+     name='kms',
+     version='1.0.0.2',
      author="Ar$h1a",
      author_email="1arhacker1@gmail.com",
      description="A Hacking Library That Helps You To Create Hacking Tools.",
      long_description=long_description,
    long_description_content_type="text/markdown",
-     url="https://github.com/ArshiaPRG/KMS",
+     url="https://github.com/ArshiaPRG/kms",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
